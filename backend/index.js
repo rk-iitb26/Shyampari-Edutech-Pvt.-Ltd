75e5e3b963e09064d9b05b2ac432c2a37daf4769
aashita-46 <aashitajolly4@gmail.com>
2026-04-06 18:18:40 +0530

    updated links

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,8 @@ app.use(cors({
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
-        "https://shyampari-edutech-pvt-ltd.vercel.app",
+        "https://shyampari-edutech-pvt-ltd-delta.vercel.app/",
+        "https://shyampari-edutech-pvt-ltd-1.onrender.com"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
